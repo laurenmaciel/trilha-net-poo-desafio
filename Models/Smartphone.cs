8250cf7324c009ff482278b9ce8f69963027a7ae
Lauren Maciel <laurenmaciel1@gmail.com>
2025-08-10 02:45:50 -0300

    versão atualizada do desafio de POO concluido

---
+++ Models/Smartphone.cs
@@ -0,0 +1,33 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        public string NumeroLigando { get; set; } 
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao(string numeroLigando)
+        {
+            NumeroLigando = numeroLigando;
+            Console.WriteLine($"Recebendo ligação de {numeroLigando}...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "9689-6524", modelo: "Modelo 1", imei: "123456789", memoria: 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.InstalarAplicativo("Instagram");
+// adicionar mais um instalar app
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero:"9354 - 4852", modelo: "Modelo 2", imei:"987654321", memoria: 512);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.InstalarAplicativo("Tik Tok");
+iphone.ReceberLigacao("3245 - 2541");
